bbe04f1e3ff38cad21b828c2da5d448d1faa8e9a
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2017-05-15 09:10:09 +0200

    -

--- source/workarounds/impl/windows_console_io/constants.hpp
+++ source/workarounds/impl/windows_console_io/constants.hpp
@@ -10,6 +10,19 @@
 
 namespace stdlib{ namespace impl{ namespace windows_console_io{
 
+    // ASCII DEL, code 127, “delete”, is here used as a replacement for encoding
+    // errors or unrepresentable code point.
+    //
+    // ASCII has a character dedicated to the purpose: SUB “substitute”, code 27, ^Z.
+    // But in Windows ^Z is used as an EOF marker for text strems. So ^Z as
+    // replacement is not usable in Windows, i.e. it's not a general solution.
+    //
+    // Unicode, an extension of ASCII, also has its own character dedicated to the
+    // purpose, namely code U+FFFD “replacement character”. It translates to at
+    // least two UTF-8 bytes so it's not trivial to check for. And it can't be
+    // represented in simple single byte encodings such as codepage 437, so it's not
+    // usable in single byte encodings, i.e. it's not a general solution.
+
     namespace ascii {
         char const      del                     = '\x7F';
     }  // namespace ascii
