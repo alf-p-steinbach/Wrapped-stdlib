1f2b508706339a143c284516e72fd3b672baea95
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2017-06-10 03:01:06 +0200

    -

--- source/all/text/cpp_strings.hpp
+++ source/all/text/cpp_strings.hpp
@@ -6,7 +6,7 @@
 
 #include <stdlib/string.hpp>        // basic_string template, string and wstring.
 
-#include <stdlib/extension/version.hpp>         // STDLIB_IS_CPP17
-#if defined( STDLIB_USE_EXPERIMENTAL_CPP17 ) || defined( STDLIB_IS_CPP17 )
+#include <stdlib/extension/version.hpp>
+#if defined( STDLIB_USE_EXPERIMENTAL_CPP17 ) || defined( STDLIB_COMPILER_SUPPORTS_CPP17 )
 #   include <stdlib/string_view.hpp>            // basic_string_view template.
 #endif
