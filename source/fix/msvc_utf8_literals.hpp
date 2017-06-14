0a77c11a9ca9a7e3c9fd4ec55ce9a0de9003773f
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2017-06-14 08:10:06 +0200

    -

---
+++ source/fix/msvc_utf8_literals.hpp
@@ -0,0 +1,17 @@
+﻿#pragma once    // Source encoding: utf-8 with BOM ∩
+// #include <stdlib/fix/utf8_literals.hpp>
+//
+// See e.g. the MSDN article on "pragma execution_character_set"
+// at <url:https://msdn.microsoft.com/en-us/library/mt708823.aspx>.
+//
+// STDLIB_NO_MSVC_UTF8_LITERALS_FIX suppresses forced UTF-8 literals.
+//
+// Copyright © 2017 Alf P. Steinbach, distributed under Boost license 1.0.
+
+#ifndef STDLIB_NO_MSVC_UTF8_LITERALS_FIX
+#   if defined( _MSC_VER )
+#       if _MSC_VER >= 1700     // Visual C++ 2015 or later
+#           pragma execution_character_set( "utf-8" )   // UTF-8 narrow literals.
+#       endif
+#   endif
+#endif
