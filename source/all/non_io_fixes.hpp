b7cc002ac84c90ffe2d656fcfb5af1729a7b0586
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2017-06-15 06:44:58 +0200

    -

---
+++ source/all/non_io_fixes.hpp
@@ -0,0 +1,9 @@
+﻿#pragma once    // Source encoding: utf-8 with BOM ∩
+// #include <stdlib/all/non_io_fixes.hpp>
+// Copyright © 2017 Alf P. Steinbach, distributed under Boost license 1.0.
+
+#include <stdlib/fix/msvc_wolfcalls_about_std_functions.hpp>     // BEST IF FIRST.
+
+#include <stdlib/fix/default_locale.hpp>
+#include <stdlib/fix/msvc_utf8_literals.hpp>
+#include <stdlib/fix/msvc_named_boolean_operators.hpp>
