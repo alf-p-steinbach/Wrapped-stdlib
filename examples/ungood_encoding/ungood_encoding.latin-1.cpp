fa7fb9d05bedacd64ba5cdce7025b332ba2a979c
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2017-06-15 10:09:47 +0200

    -

---
+++ examples/ungood_encoding/ungood_encoding.latin-1.cpp
@@ -0,0 +1,10 @@
+// Source encoding: Latin-1
+// Works by default with MSVC, gibberish with g++.
+#include <stdlib/all/basics.hpp>        // Used here: <iostream> and <iomanip>
+using namespace std;
+
+auto main()
+    -> int
+{
+    cout << "Everybody likes Norwegian blåbærsyltetøy!" << endl;
+}
---
+++ examples/ungood_encoding/ungood_encoding.utf-8_sans_bom.cpp
@@ -0,0 +1,10 @@
+// Source encoding: utf-8 sans BOM
+// Works by default with g++, gibberish with MSVC.
+#include <stdlib/all/basics.hpp>        // Used here: <iostream> and <iomanip>
+using namespace std;
+
+auto main()
+    -> int
+{
+    cout << "Everybody likes Norwegian blåbærsyltetøy!" << endl;
+}
