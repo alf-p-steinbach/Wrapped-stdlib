3c23eca337a870f01e4a2a35ae7e4f25e8d898d3
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2017-06-19 12:51:19 +0200

    Completed C+14 §25 “Algorithms library” sub-library

---
+++ source/all/of_sublibrary/algorithms.hpp
@@ -0,0 +1,16 @@
+﻿#pragma once    // Source encoding: utf-8 with BOM ∩
+// #include <stdlib/all/of_sublibrary/algorithms.hpp>
+//
+// The C++ standard's section “Algorithms library” (in C++14 that's §25).
+// The set of headers included here are those in C++14 §25.1/2 table 112.
+//
+// Copyright © 2017 Alf P. Steinbach, distributed under Boost license 1.0.
+
+
+// Non-modifying sequence operations
+// Mutating sequence operations
+// Sorting and related operations
+#include <stdlib/algorithm.hpp>
+
+// C library algorithms 
+#include <stdlib/c/stdlib.hpp>
