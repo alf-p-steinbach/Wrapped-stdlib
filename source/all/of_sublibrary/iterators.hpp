eb5e579ca2b2c66051b611b8cc97da5948e3e5ff
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2017-06-19 12:47:14 +0200

    Completed C++14 §24 “Iterators library” sub-library

---
+++ source/all/of_sublibrary/iterators.hpp
@@ -0,0 +1,13 @@
+﻿#pragma once    // Source encoding: utf-8 with BOM ∩
+// #include <stdlib/all/of_sublibrary/iterators.hpp>
+//
+// The C++ standard's section “Iterators library” (in C++14 that's §24).
+// The set of headers included here are those in C++14 §24.1/2 table 104.
+//
+// Copyright © 2017 Alf P. Steinbach, distributed under Boost license 1.0.
+
+
+// Iterator primitives
+// Predefined iterators
+// Stream iterators
+#include <stdlib/iterator.hpp>
