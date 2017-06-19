aeebe7db7718c63f998875203d49ee0e6c8ddd5e
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2017-06-19 13:26:49 +0200

    Completed C++14 §27 “Input/output library” sub-library

---
+++ source/all/of_sublibrary/input_output.hpp
@@ -0,0 +1,33 @@
+﻿#pragma once    // Source encoding: utf-8 with BOM ∩
+// #include <stdlib/all/of_sublibrary/input_output.hpp>
+//
+// The C++ standard's section “Input/output library” (in C++14 that's §27).
+// The set of headers included here are those in C++14 §27.1/2 table 121.
+//
+// Copyright © 2017 Alf P. Steinbach, distributed under Boost license 1.0.
+
+
+// Forward declarations 
+#include <stdlib/ioswfd.hpp>
+
+// Standard iostream objects
+#include <stdlib/iostream.hpp>
+
+// Iostreams base classes
+#include <stdlib/ios.hpp>
+
+// Stream buffers
+#include <stdlib/streambuf.hpp>
+
+// Formatting and manipulators
+#include <stdlib/istream.hpp>
+#include <stdlib/ostream.hpp>
+#include <stdlib/iomanip.hpp>
+
+// String streams
+#include <stdlib/sstream.hpp>
+
+// File streams
+#include <stdlib/fstream.hpp>
+#include <stdlib/c/stdio.hpp>
+#include <stdlib/c/inttypes.hpp>
