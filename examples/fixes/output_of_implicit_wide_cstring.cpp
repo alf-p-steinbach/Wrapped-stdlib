07502b9f9f58bef542c25f17e72347c192d74f8a
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2017-06-19 18:16:14 +0200

    Example of the fix of output to wide stream of an implicit wide C-string

---
+++ examples/fixes/output_of_implicit_wide_cstring.cpp
@@ -0,0 +1,18 @@
+﻿// Source encoding: utf-8 with BOM ∩
+//
+// Define STDLIB_NO_OUTPUT_OF_IMPLICIT_WIDE_C_STRING_FIX to avoid the fix.
+// Without the fix the address of the string is presented, instead of text.
+
+#include <stdlib/all/basics.hpp>
+using namespace std;
+using namespace stdlib::type_builders;      // ptr_
+
+struct Implicit_wide_string
+{
+    operator ptr_<const wchar_t>() const { return L"Blåbærsyltetøy!"; }
+};
+
+auto main() -> int
+{
+    wcout << Implicit_wide_string{} << endl;
+}
