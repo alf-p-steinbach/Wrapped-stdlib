f1ff95a61737a75a8cd12bada35370e8b3dac9e5
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2017-06-19 12:27:45 +0200

    Completed C++14 §22 “Localization library” sub-library

---
+++ source/all/of_sublibrary/localization.hpp
@@ -0,0 +1,19 @@
+﻿#pragma once    // Source encoding: utf-8 with BOM ∩
+// #include <stdlib/all/of_sublibrary/localization.hpp>
+//
+// The C++ standard's section “Localization library” (in C++14 that's §22).
+// The set of headers included here are those in C++14 §22.1/2 table 80.
+//
+// Copyright © 2017 Alf P. Steinbach, distributed under Boost license 1.0.
+
+
+// Locales
+// Standard locale Categories
+#include <stdlib/locale.hpp>
+
+// Standard code conversion facets
+#include <stdlib/codecvt.hpp>
+
+// C library locales
+#include <stdlib/c/locale.hpp>
+
