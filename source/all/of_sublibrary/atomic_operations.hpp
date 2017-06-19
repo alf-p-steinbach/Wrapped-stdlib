c2f87db01be569b35a4ffb5d55bd8dba808ef9db
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2017-06-19 13:30:59 +0200

    Completed C++14 §29 “Atomic operations library” sub-library

---
+++ source/all/of_sublibrary/atomic_operations.hpp
@@ -0,0 +1,16 @@
+﻿#pragma once    // Source encoding: utf-8 with BOM ∩
+// #include <stdlib/all/of_sublibrary/atomic_operations.hpp>
+//
+// The C++ standard's section “Atomic operations library” (in C++14 that's §29).
+// The set of headers included here are those in C++14 §29.1/2 table 145.
+//
+// Copyright © 2017 Alf P. Steinbach, distributed under Boost license 1.0.
+
+
+// Order and Consistency
+// Lock-free Property
+// Atomic Types
+// Operations on Atomic Types
+// Flag Type and Operations
+// Fences
+#include <stdlib/atomic.hpp>
