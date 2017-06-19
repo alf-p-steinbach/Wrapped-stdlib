ff423a4efa30d493738aed92ca6dfd057fda50e7
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2017-06-19 15:34:11 +0200

    Example for console i/o fix

--- examples/fixed/console_io.cpp
+++ examples/fixed/console_io.cpp
@@ -1,4 +1,8 @@
 ﻿// Source encoding: utf-8 with BOM ∩
+//
+// Define STDLIB_NO_CONSOLE_IO_FIX to turn off the console i/o fix.
+// Define STDLIB_NO_MSVC_UTF8_LITERALS_FIX to turn off forced UTF-8 literals.
+
 #include <stdlib/iostream.hpp>
 using namespace std;
 
