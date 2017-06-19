1c369ed2f86723ab958877532ae502ba9873e431
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2017-06-19 13:19:43 +0200

    Completed C++14 §28 “Regular expressions library” sub-library

---
+++ source/all/of_sublibrary/regular_expressions.hpp
@@ -0,0 +1,18 @@
+﻿#pragma once    // Source encoding: utf-8 with BOM ∩
+// #include <stdlib/all/of_sublibrary/regular_expressions.hpp>
+//
+// The C++ standard's section “Regular expressions library” (in C++14 that's §28).
+// The set of headers included here are those in C++14 §28.1/2 table 136.
+//
+// Copyright © 2017 Alf P. Steinbach, distributed under Boost license 1.0.
+
+
+// Constants
+// Exception type
+// Traits
+// Regular expression template <regex>
+// Submatches
+// Match results
+// Algorithms
+// Iterators
+#include <stdlib/regex.hpp>
