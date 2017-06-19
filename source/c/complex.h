13f972c63c38810728991f42e302fb0a3e3d52df
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2017-06-19 14:00:41 +0200

    Completed C library headers as per C++14 §D.5/1 table 155

---
+++ source/c/complex.h
@@ -0,0 +1,8 @@
+﻿#pragma once    // Source encoding: utf-8 with BOM ∩
+// #include <stdlib/c/complex.hpp>
+//
+// The effect is AS IF the C++ (not C) header `<complex>` were included.
+// Copyright © 2017 Alf P. Steinbach, distributed under Boost license 1.0.
+
+#include <ccomplex>
+#include <complex.h>
---
+++ source/c/iso646.h
@@ -0,0 +1,8 @@
+﻿#pragma once    // Source encoding: utf-8 with BOM ∩
+// #include <stdlib/c/iso646.hpp>
+//
+// As if each name from <ciso646> is placed in global namespace.
+// Copyright © 2017 Alf P. Steinbach, distributed under Boost license 1.0.
+
+#include <ciso646>
+#include <iso646.h>
