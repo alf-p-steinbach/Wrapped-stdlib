84f695cb1cd3ac01e619aa472ac5126c7e68e0cd
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2017-06-19 12:22:06 +0200

    Completed C++15 §21 “Strings library” sub-library

---
+++ source/all/of_sublibrary/strings.hpp
@@ -0,0 +1,20 @@
+﻿#pragma once    // Source encoding: utf-8 with BOM ∩
+// #include <stdlib/all/of_sublibrary/strings.hpp>
+//
+// The C++ standard's section “Strings library” (in C++14 that's §21).
+// The set of headers included here are those in C++14 §21.1/2 table 61.
+//
+// Copyright © 2017 Alf P. Steinbach, distributed under Boost license 1.0.
+
+
+// Character traits
+// String classes
+#include <stdlib/string.hpp>
+
+// Null-terminated sequence utilities
+#include <stdlib/c/ctype.hpp>
+#include <stdlib/c/wctype.hpp>
+#include <stdlib/c/string.hpp>
+#include <stdlib/c/wchar.hpp>
+#include <stdlib/c/stdlib.hpp>
+#include <stdlib/c/uchar.hpp>
