46f38b102c64253a1dbe95920e80d4290c0d61d9
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2017-06-20 08:01:40 +0200

    Example of the support for using ANSI escape sequences in Windows 10 (or later) consoles

---
+++ examples/fixes/ansi_escape_sequences.cpp
@@ -0,0 +1,21 @@
+﻿// Source encoding: utf-8 with BOM ∩
+//
+// Define STDLIB_NO_MATH_CONSTANTS to turn off inclusion of the constants.
+//
+// For documentation of the sequences supported by Windows 10 console windows, see
+// the article titled “Console Virtual Terminal Sequences” in MSDN Library
+// (Microsoft Developer Network Library), which was available at
+// <url: https://msdn.microsoft.com/en-us/library/windows/desktop/mt638032(v=vs.85).aspx>
+// at the time when I this example program was written, medio June 2017.
+
+#include <stdlib/all/basics.hpp>
+using namespace std;
+
+auto main() -> int
+{
+    const auto s =
+        "\x1B[37mColored text is fun.\n"
+        "Look, here’s some \x1B[91mred text\x1B[37m!\n"
+        "And here’s some \x1B[92mgreen text\x1B[37m, amazing!";
+    cout << s << endl;
+}
