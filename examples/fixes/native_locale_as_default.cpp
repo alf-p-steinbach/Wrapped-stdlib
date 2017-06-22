b1df8446e74e3f39a702aa7a4d99cd6ef37fba69
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2017-06-22 13:56:36 +0200

    -

--- examples/fixes/native_locale_as_default.cpp
+++ examples/fixes/native_locale_as_default.cpp
@@ -1,7 +1,7 @@
 ﻿// Source encoding: utf-8 with BOM ∩
 //
-// Define STDLIB_NO_DEFAULT_C_LOCALE_FIX to suppresses the C locale config.
-// Define STDLIB_NO_DEFAULT_CPP_LOCALE_FIX to suppresses the C++ locale config.
+// Define STDLIB_NO_DEFAULT_C_LOCALE_FIX to suppress the C locale config.
+// Define STDLIB_NO_DEFAULT_CPP_LOCALE_FIX to suppress the C++ locale config.
 //
 // Note: for wide stream console output this is mostly about *nix-land. In
 // Windows the console i/o fix takes care of wide stream console output.
