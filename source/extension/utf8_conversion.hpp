6562c378d9724da8ca1da02844d24a401cdcf29b
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2018-03-06 16:39:58 +0100

    Added inline namespace utf8_conversion

--- source/extension/utf8_conversion.hpp
+++ source/extension/utf8_conversion.hpp
@@ -17,55 +17,58 @@ namespace stdlib{ namespace ext{
     using std::string;
     using std::wstring;
 
-    inline auto utf8_from( const wstring& s )
-        -> string
-    {
-        string                              result;
-        Streaming_wide_to_byte_converter    converter;
-
-        const Size n = s.length();
-        for( Index read_pos = 0; read_pos < n or converter.n_buffered() > 0; )
+    inline namespace utf8_conversion {
+        inline auto utf8_from( const wstring& s )
+            -> string
         {
-            char out_buffer[stdlib::impl::converter_buffer_size];
-            const Size max_n_bytes = array_size( out_buffer );
+            string                              result;
+            Streaming_wide_to_byte_converter    converter;
 
-            const Size n_available_wchars = n - read_pos;
-            const Size max_n_wchars = min(
-                n_available_wchars, converter.available_space()
-                );
+            const Size n = s.length();
+            for( Index read_pos = 0; read_pos < n or converter.n_buffered() > 0; )
+            {
+                char out_buffer[stdlib::impl::converter_buffer_size];
+                const Size max_n_bytes = array_size( out_buffer );
 
-            converter.add( max_n_wchars, s.data() + read_pos );
-            const Size n_bytes = converter.convert_into( out_buffer, max_n_bytes );
-            result.append( out_buffer, n_bytes );
+                const Size n_available_wchars = n - read_pos;
+                const Size max_n_wchars = min(
+                    n_available_wchars, converter.available_space()
+                    );
 
-            read_pos += max_n_wchars;
+                converter.add( max_n_wchars, s.data() + read_pos );
+                const Size n_bytes = converter.convert_into( out_buffer, max_n_bytes );
+                result.append( out_buffer, n_bytes );
+
+                read_pos += max_n_wchars;
+            }
+            return result;
         }
-        return result;
-    }
 
-    inline auto wide_from_utf8( string const& s )
-        -> wstring
-    {
-        wstring                             result;
-        Streaming_byte_to_wide_converter    converter;
-
-        const Size n = s.length();
-        for( Index read_pos = 0; read_pos < n or converter.n_buffered() > 0; )
+        inline auto wide_from_utf8( string const& s )
+            -> wstring
         {
-            wchar_t out_buffer[stdlib::impl::converter_buffer_size];
-            const Size max_n_wchars = array_size( out_buffer );
+            wstring                             result;
+            Streaming_byte_to_wide_converter    converter;
 
-            const Size n_available_bytes = n - read_pos;
-            const Size max_n_bytes = min(
-                n_available_bytes, converter.available_space()
-                );
+            const Size n = s.length();
+            for( Index read_pos = 0; read_pos < n or converter.n_buffered() > 0; )
+            {
+                wchar_t out_buffer[stdlib::impl::converter_buffer_size];
+                const Size max_n_wchars = array_size( out_buffer );
 
-            converter.add( max_n_bytes, s.data() + read_pos );
-            const Size n_wchars = converter.convert_into( out_buffer, max_n_wchars );
-            result.append( out_buffer, n_wchars );
+                const Size n_available_bytes = n - read_pos;
+                const Size max_n_bytes = min(
+                    n_available_bytes, converter.available_space()
+                    );
 
-            read_pos += max_n_bytes;
+                converter.add( max_n_bytes, s.data() + read_pos );
+                const Size n_wchars = converter.convert_into( out_buffer, max_n_wchars );
+                result.append( out_buffer, n_wchars );
+
+                read_pos += max_n_bytes;
+            }
+            return result;
         }
-        return result;
-    }
+    }  // namespace utf8_conversion
+
 }}  // namespace stdlib::ext
