261c5bb87e270147026f184d03a18c986830c852
Alf P. Steinbach <alf.p.steinbach@gmail.com>
2018-07-16 08:34:28 +0200

    _

--- source/variant.hpp
+++ source/variant.hpp
@@ -1,27 +1,27 @@
 ﻿#pragma once    // Source encoding: utf-8 with BOM ∩
-// #include <stdlib/optional.hpp>
+// #include <stdlib/variant.hpp>
 //
-// A boxed value, where the box can be empty. Conceptual view: pointer.
+// A discriminated union, essentially a boxed value of one of N specified types.
 // Copyright © 2017 Alf P. Steinbach, distributed under Boost license 1.0.
 
 #include <stdlib/extension/version.hpp>         // STDLIB_COMPILER_SUPPORTS_CPP17
 #include <stdlib/fix/msvc_wolfcalls_about_std_functions.hpp>
 
 #if STDLIB_USE_EXPERIMENTAL_CPP17
-#   include <experimental/optional>
+#   include <experimental/variant>
 #elif STDLIB_COMPILER_SUPPORTS_CPP17
-#   include <optional>
+#   include <variant>
 #else
 #   ifdef _MSC_VER
-#       error "<optional> is apparently not available (use option `/std:c++latest`?)."
+#       error "<variant> is apparently not available (use option `/std:c++latest`?)."
 #   else
-#       error "<optional> appears not available (try define STDLIB_USE_EXPERIMENTAL_CPP17?)."
+#       error "<variant> appears not available (try define STDLIB_USE_EXPERIMENTAL_CPP17?)."
 #   endif
 #endif
 
-#ifdef STDLIB_HOIST_UP_OPTIONAL_TEMPLATE
+#ifdef STDLIB_HOIST_UP_VARIANT_TEMPLATE
     namespace std {
-        using experimental::optional;
+        using experimental::variant;
     }  // namespace std;
 #endif
 
